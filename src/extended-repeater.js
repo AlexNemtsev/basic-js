4d0e5ada973bd199a3ee60401044c21852a198a0
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2022-10-10 10:51:11 +0300

    feat: repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const arr = [];
+  const adds = [];
+  let addStr = '';
+  const rT = (typeof options.repeatTimes === 'undefined' ? 1 : options.repeatTimes);
+  const aRT = (typeof options.additionRepeatTimes === 'undefined' ? 1 : options.additionRepeatTimes);
+  const separator = (typeof options.separator === 'undefined' ? '+' : options.separator);
+  const addSeparator = (typeof options.additionSeparator === 'undefined' ? '|' : options.additionSeparator);
+
+  if (typeof options.addition !== 'undefined') {
+    for (let i = 0; i < aRT; i++) {
+      adds.push(options.addition);
+    }
+
+    addStr = (adds.length === 1 ? (adds[0]) : adds.join(addSeparator));
+  }
+
+  for (let i = 0; i < rT; i++) {
+    arr.push(str + addStr);
+  }
+
+  return arr.length === 1 ? arr[0] : arr.join(separator);
 }
 
 module.exports = {
