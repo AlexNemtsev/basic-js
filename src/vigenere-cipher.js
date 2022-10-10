80d82e69e2dd36c956f3b5ee7b50009aae60a1fe
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2022-10-10 14:05:51 +0300

    feat: vigenere solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  constructor(isDirect = true) {
+    this.isReversed = !isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(...rest) {
+    if (rest.length < 2 || rest.includes(undefined)) throw new Error('Incorrect arguments!');
+
+    const msg = rest[0].toUpperCase();
+    const key = rest[1].toUpperCase();
+    let enc = '';
+    let i = 0;
+
+    for (let char of msg) {
+      if (this.alphabet.includes(char)) {
+        const shift = key.charCodeAt(i) - this.alphabet.charCodeAt(0);
+        const shiftedCode = char.charCodeAt(0) + shift;
+        const shifted = String.fromCharCode(shiftedCode > 90 ? shiftedCode + 6 : shiftedCode).toUpperCase();
+
+        enc += shifted;
+
+        i = (i + 1) % key.length;
+      } else {
+        enc += char;
+      }
+    }
+
+    return this.isReversed ? enc.split('').reverse().join('') : enc;
+  }
+
+  decrypt(...rest) {
+    if (rest.length < 2 || rest.includes(undefined)) throw new Error('Incorrect arguments!');
+    
+    const msg = rest[0].toLowerCase();
+    const key = rest[1].toUpperCase();
+    let dec = '';
+    let i = 0;
+
+    for (let char of msg) {
+      if (this.alphabet.toLowerCase().includes(char)) {
+        const shift = key.charCodeAt(i) - this.alphabet.charCodeAt(0);
+        const shiftedCode = char.charCodeAt(0) - shift;
+        const shifted = String.fromCharCode(shiftedCode < 97 ? shiftedCode - 6 : shiftedCode).toUpperCase();
+
+        dec += shifted;
+
+        i = (i + 1) % key.length;
+      } else {
+        dec += char;
+      }
+    }
+
+    return this.isReversed ? dec.split('').reverse().join('') : dec;
   }
 }
 
